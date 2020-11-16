9735b04422400388e8cf3629403ca3a64375bca3
sandeep-v1404 <valpasanisandeep@gmail.com>
2020-11-16 13:08:38 +0530

    feat(Firebase):Updated the Function with async and await

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -46,7 +46,38 @@ exports.addRequest = functions.https.onCall((data, content) => {
 
 
 //Promises Function
-exports.upvote = functions.https.onCall((data, context) => {
+// exports.upvote = functions.https.onCall((data, context) => {
+//     if (!context.auth) {
+//         throw new functions.https.HttpsError(
+//             'unauthenticated',
+//             'Only authenticated users can add requests'
+//         );
+//     }
+//     //Get Refs for user doc and request doc
+//     const user = admin.firestore().collection('users').doc(context.auth.uid);
+//     const request = admin.firestore().collection('requests').doc(data.id);
+//     return user.get().then(doc => {
+//         //Check user hasn't already upvoted the requests
+//         if (doc.data().upvotedOn.includes(data.id)) {
+//             throw new functions.https.HttpsError(
+//                 'failed-precondition',
+//                 'You can only upvote something once'
+//             );
+//         }
+//         //Update user array
+//         // eslint-disable-next-line promise/no-nesting
+//         return user.update({
+//             upvotedOn: [...doc.data().upvotedOn, data.id]
+//         }).then(() => {
+//             //Update votes on the requests
+//             return request.update({
+//                 upvotes: admin.firestore.FieldValue.increment(1)
+//             });
+//         });
+//     });
+// });
+
+exports.upvote = functions.https.onCall(async(data, context) => {
     if (!context.auth) {
         throw new functions.https.HttpsError(
             'unauthenticated',
@@ -56,26 +87,27 @@ exports.upvote = functions.https.onCall((data, context) => {
     //Get Refs for user doc and request doc
     const user = admin.firestore().collection('users').doc(context.auth.uid);
     const request = admin.firestore().collection('requests').doc(data.id);
-    return user.get().then(doc => {
-        //Check user hasn't already upvoted the requests
-        if (doc.data().upvotedOn.includes(data.id)) {
-            throw new functions.https.HttpsError(
-                'failed-precondition',
-                'You can only upvote something once'
-            );
-        }
-        //Update user array
-        // eslint-disable-next-line promise/no-nesting
-        return user.update({
-            upvotedOn: [...doc.data().upvotedOn, data.id]
-        }).then(() => {
-            //Update votes on the requests
-            return request.update({
-                upvotes: admin.firestore.FieldValue.increment(1)
-            });
-        });
+    const doc = await user.get();
+    //Check user hasn't already upvoted the requests
+    if (doc.data().upvotedOn.includes(data.id)) {
+        throw new functions.https.HttpsError(
+            'failed-precondition',
+            'You can only upvote something once'
+        );
+    }
+    //Update user array
+    // eslint-disable-next-line promise/no-nesting
+    await user.update({
+        upvotedOn: [...doc.data().upvotedOn, data.id]
+    });
+    //Update votes on the requests
+    return request.update({
+        upvotes: admin.firestore.FieldValue.increment(1)
     });
 });
+
+
+
 
 //     return new Promise((resolve, reject) => {
 //         user.get().then(doc => {
